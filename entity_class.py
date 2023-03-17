4f464f936b23ddc701711227714623d946cb45ab
Artur <artzhemch@mail.ru>
2023-03-17 03:22:57 +0300

    Классы героя и врагов. Снаряды и столкновения

--- entity_class.py
+++ entity_class.py
@@ -10,45 +10,63 @@ class Entity(pygame.sprite.Sprite):
     all_sprites = None  # Все спрайты
     sprite_groups = defaultdict(pygame.sprite.Group)  # Группы спрайтов по alliance
 
-    def __init__(self, *group, alliance: int = 0, x: int, y: int, v_x: int = 0, v_y: int = 0):
-        """alliance: какой команде принадлежит объект
-        x, y: координаты исходной точки
-        v_x, v_y: скорости"""
+    def __init__(self,
+                 *group,
+                 alliance: int = 0,
+                 x: int,
+                 y: int,
+                 v_x: int = 0,
+                 v_y: int = 0,
+                 hp: int = -1,
+                 collision_damage: int = 0):
+        """alliance: какой команде принадлежит объект,
+        x, y: координаты исходной точки,
+        v_x, v_y: скорости,
+        hp: сколько урона может выдержать,
+        collision_dmg: урон от столкновения"""
         super().__init__(*group)
-        print(self.__class__)
         if not self.__class__.image:
             self.__class__.image = load_image(self.__class__.image_name)
         self.add(Entity.sprite_groups[alliance])
-        if not Entity.all_sprites:
-            Entity.all_sprites = group
+        # if not Entity.all_sprites:
+        #     Entity.all_sprites = pygame.sprite.Group(group)
         self.image = self.__class__.image
         self.rect = self.image.get_rect()
         self.rect.x, self.rect.y = x, y
         self.v_x = v_x
         self.v_y = v_y
         self.alliance = alliance
+        self.collision_damage = collision_damage
+        self.hp = hp
 
     def update(self):
         self.rect = self.rect.move(self.v_x, self.v_y)
         for ally, sprite_group in Entity.sprite_groups.items():
             if ally == self.alliance:
-                pass
-                #continue
+                continue
             collides = pygame.sprite.spritecollide(self, sprite_group, False)
-            if len(collides) > 1:
+            if len(collides) > 0:
                 for i in collides:
-                    print(i)
                     self.get_hit(i)
                 #self.rect = self.rect.move(10, 10)
 
     def get_hit(self, other):
+        "Столкновение с другим объектом"
         if self is other:
             return
         self.rect = self.rect.move(1, 0)
-        pass
-        #for i in pygame.sprite.spritecollide(self, Entity.all_sprites, False):
-        #    print(i)
+        self.receive_damage(other.collision_damage)
+        other.receive_damage(self.collision_damage)
 
+    def receive_damage(self, damage: int):
+        self.hp -= damage
+        if self.hp <= 0:
+            self.get_destroyed()
+
+    def get_destroyed(self):
+        """Уничтожение объекта"""
+        Entity.sprite_groups[self.alliance].remove(self)
+        Entity.all_sprites.remove(self)
 
 
 
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 import os
 import sys
 from entity_class import Entity
-from plane_class import Plane
+from plane_class import Plane, Player
 from constants import BACKGROUND_COLOR, WIDTH, HEIGHT
 
 
@@ -12,16 +12,19 @@ def main():
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode(size)
     all_sprites = pygame.sprite.Group()
-    for i in range(3):
-        # Entity(all_sprites)
-        Plane(all_sprites, x=100, y=100 * i, v_x=0, v_y=-3 + 2 * i)
-    Plane(all_sprites, x=100, y=100, v_x=0, v_y=5)
+    Entity.all_sprites = all_sprites
 
+    Plane(all_sprites, x=100, y=400)
+    Plane(all_sprites, alliance=-1, x=100, y=100, v_x=0, v_y=5)
+    player = Player(all_sprites, x=0, y=0)
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.MOUSEMOTION:
+                x, y = event.pos
+                player.rect.topleft = x - 50, y - 50
         screen.fill(pygame.Color(BACKGROUND_COLOR))
         all_sprites.draw(screen)
         all_sprites.update()
